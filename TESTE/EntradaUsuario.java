125ed8dd796f975eeffb49a8fee8fa6c37f13d22
natachadpduarte <natachadpduarte@gmail.com>
2021-02-06 15:56:52 -0300

    projeto

---
+++ TESTE/EntradaUsuario.java
@@ -0,0 +1,19 @@
+package Empresa;
+
+import java.util.Scanner;
+
+public class EntradaUsuario {
+	private static Scanner leitura = new Scanner(System.in);
+	
+	//leitura de dados, observando que o tratamento com try catch tem que ser feito na classe chamadora 
+	public static float getFloat() {
+		return Float.parseFloat(EntradaUsuario.leitura.nextLine());
+		
+	}
+	public static  int getInt() {
+		return Integer.parseInt(EntradaUsuario.leitura.nextLine());
+		
+	}
+	
+	
+}
