46d20cecf06c3965516df92116bf90c9cae1184c
natachadpduarte <natachadpduarte@gmail.com>
2021-02-07 14:26:53 -0300

    ProjetoII

--- Projeto2/Anexo2Industria.java
+++ Projeto2/Anexo2Industria.java
@@ -1,40 +1,46 @@
 package empresa;
 
-
-	public class AnexoII extends Empresa{
-
-
+public class Anexo2Industria extends Empresa{
+		
 		public void run() {
 			this.digitaFaturamento();
 			this.calculaImposto();
 			SaidaUsuario.relatorio(getFaturamento(), getFaturamentoTotal(), getImposto());
 		}
-
+		
+		
 		@Override
 		public void calculaImposto() {
 			// TODO Auto-generated method stub
 			this.setFaturamentoTotal();
-			//float mes12 = this.getFaturamentoTotal().get(11);
 			float mes12 = this.getFaturamento().get(11);
-
-			if (this.getFaturamentoTotal() >=0 && this.getFaturamentoTotal() <= 180000f) {
+			
+			if (this.getFaturamentoTotal() >= 0 && this.getFaturamentoTotal() <= 180000.00f) {
 				this.setImposto(mes12 * 0.045f);
 			}
-			else if (this.getFaturamentoTotal()>180000f && this.getFaturamentoTotal()<=360000.00f) {
+			
+			
+			else if (this.getFaturamentoTotal() > 180000.00f && this.getFaturamentoTotal() <= 360000.00f) {
 				this.setImposto(mes12 * 0.078f);
+						
 			}
-			else if (this.getFaturamentoTotal()>360000f && this.getFaturamentoTotal()<=720000.00f) {
+			
+			else if (this.getFaturamentoTotal() > 360000.00f && this.getFaturamentoTotal() <= 720000.00f) {
 				this.setImposto(mes12 * 0.10f);
+						
 			}
-			else if (this.getFaturamentoTotal()>720000f && this.getFaturamentoTotal()<=180000.00f) {
+			
+			else if (this.getFaturamentoTotal() > 720000.00f && this.getFaturamentoTotal() <= 1800000.00f) {
 				this.setImposto(mes12 * 0.112f);
 			}
-			else if (this.getFaturamentoTotal()>180000f && this.getFaturamentoTotal()<=3600000.00f) {
+			
+			else if (this.getFaturamentoTotal() > 1800000.00f && this.getFaturamentoTotal() <= 3600000.00f) {
 				this.setImposto(mes12 * 0.147f);
 			}
-			else if (this.getFaturamentoTotal()>3600000f && this.getFaturamentoTotal()<=4800000.00f) {
+			
+			else if (this.getFaturamentoTotal() > 3600000.00f && this.getFaturamentoTotal() <= 4800000.00f) {
 				this.setImposto(mes12 * 0.30f);
-			}			
+}			
 			
 			else if (this.getFaturamentoTotal() > 4800000)
 			{
