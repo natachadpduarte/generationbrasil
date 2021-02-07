057871d13b626252e035cf025759affd6e4a2d25
natachadpduarte <natachadpduarte@gmail.com>
2021-02-07 16:57:19 -0300

    ProjetoII

--- Projeto2/SaidaUsuario.java
+++ Projeto2/SaidaUsuario.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class SaidaUsuario {
 	//static Scanner = new Sc
 	public static void qualAnexo() {
-		String menuAnexo = "1- Anexo I - Comércio\r\n"
+		String menuAnexo = "\n1- Anexo I - Comércio\r\n"
 				  + "2- Anexo II - Industria\r\n"
 				  + "3- Anexo III - Serviço\r\n"
 				  + "4- Anexo IV – Construção Civil\r\n"
@@ -15,10 +15,10 @@ public class SaidaUsuario {
 	
 	public static void relatorio(List<Float> pagamento, float total, float imposto) {
 		for (int i = 0; i <= 11; i++) {
-			System.out.printf("Faturamento mes %d foi de: %.2f\n", i+1, pagamento.get(i) );	
+			System.out.printf("Faturamento mes %d foi de: R$ %.2f\n", i+1, pagamento.get(i) );	
 		}
-		System.out.println("\nSeu faturamento total dos ultimos 12 meses foi de: "+total + " reais");
-		System.out.println("\nSeu imposto do mês é de: "+imposto + " reais");
+		System.out.printf("\nSeu faturamento total dos ultimos 12 meses foi de: R$ %.2f\n\n",total);
+		System.out.printf("\nSeu imposto do mês é de: R$ %.2f\n\n",imposto);
 		
 		if (total < 0)
 		{
@@ -33,13 +33,13 @@ public class SaidaUsuario {
 		else if (total > 4800000)
 		{
 			System.out.printf("\nUhul! Parece que você está voando!!! \r\n"
-					+ " Você excedeu seu limite de faturamento para o Simples Nacional. Fique atento e converse com seu contador!  Pois precisa se enquadrar direitinho!\r\n"
+					+ "Você excedeu seu limite de faturamento para o Simples Nacional. Fique atento e converse com seu contador!  Pois precisa se enquadrar direitinho!\r\n"
 					+ "");
 		}
 		else if (total <=81000)
 		{
 			System.out.printf("\nUhul! Parece que você está crescendo!!! \r\n"
-					+ " Mas sabia que talvez você esteja pagando mais imposto do que deveria?! Converse com seu contador, pois você pode ser um Microempreendedor individual e pagar menos imposto! ");
+					+ "Mas sabia que talvez você esteja pagando mais imposto do que deveria?! Converse com seu contador, pois você pode ser um Microempreendedor individual e pagar menos imposto! ");
 		}
 		
 		
